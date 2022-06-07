b7fab31faac38d2c3393b81bea3725be4863c70e
Shiwam Tiwari <zaqqst@gmail.com>
2022-06-07 16:25:06 +0530

    trying prod

--- src/components/common/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Heading(props) {
-  return (
-    <div>
-        <h1>{props.Heading}</h1>
-    </div>
-  )
-}
--- src/components/common/List.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function List(props) {
-  return (
-    <div>{props.data.map(e=>
-    <li>{e}</li>
-    )}</div>
-  )
-}
